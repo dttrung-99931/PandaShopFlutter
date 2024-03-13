b7ec397020dc94356cea59d3309542b13322c759
ptt <ptt99931.work@gmail.com>
2024-03-13 23:02:33 +0700

    [config] add main_dev_home build config

---
+++ lib/main_dev_home.dart
@@ -0,0 +1,17 @@
+import 'package:evievm_app/src/config/app_config.dart';
+import 'package:evievm_app/app.dart';
+
+void main() async {
+  AppConfig.set(
+    appName: 'EVM Home',
+    flavorName: AppFlavor.DEV,
+    apiUrl: 'https://localhost:5001',
+    resourceIcon: 'https://evi-gonosen.s3.ap-northeast-1.amazonaws.com',
+    logResponse: true,
+    logRequest: true,
+    logBloc: true,
+    socketUrl: 'https://api.evm.gonosen.work',
+  );
+
+  await appMain();
+}
