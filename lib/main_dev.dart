69068e820e1547eb15114b815d9cd4b5914fd8d2
Đoàn Thanh Trung <doanthanhtrung4321@gmail.com>
2024-03-18 09:09:28 +0700

    [config] fix main_dev

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -3,9 +3,9 @@ import 'package:evievm_app/app.dart';
 
 void main() async {
   AppConfig.set(
-    appName: 'EVM Home',
+    appName: 'EVM Dev',
     flavorName: AppFlavor.DEV,
-    apiUrl: 'https://localhost:5001',
+    apiUrl: 'http://localhost:44444',
     resourceIcon: 'https://evi-gonosen.s3.ap-northeast-1.amazonaws.com',
     logResponse: true,
     logRequest: true,
