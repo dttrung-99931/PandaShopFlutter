66ec3920f9cfb0d49b7ccf3933180e03a01741df
ptt <ptt99931.work@gmail.com>
2024-05-08 23:26:38 +0700

    [noti] handle click orer noti

--- lib/src/features/notification/presentation/bloc/push_notification/push_notification_bloc.dart
+++ lib/src/features/notification/presentation/bloc/push_notification/push_notification_bloc.dart
@@ -7,6 +7,7 @@ import 'package:evievm_app/core/base_bloc/base_bloc.dart';
 import 'package:evievm_app/core/base_bloc/base_event.dart';
 import 'package:evievm_app/core/base_bloc/base_state.dart';
 import 'package:evievm_app/src/config/di/injection.dart';
+import 'package:evievm_app/src/features/notification/data/models/response/push_notification/push_notification_payload.dart';
 import 'package:evievm_app/src/features/notification/domain/dtos/push_notification/push_notification_dto.dart';
 import 'package:evievm_app/src/features/notification/domain/use_cases/push_notification_usecases.dart';
 import 'package:evievm_app/src/features/notification/presentation/bloc/push_notification/base/base_notification_receiver_bloc.dart';
@@ -23,6 +24,7 @@ class PushNotificationBloc extends BaseBloc {
   PushNotificationBloc(this._pushNotiUseCases) : super(InitialState()) {
     on<OnConfigNotification>(_onConfigPushNotification);
     on<OnPushNotification>(_onPushNotification);
+    on<OnPushNotificationPressed>(_onPushNotificationPressed);
   }
   final PushNotificationUseCases _pushNotiUseCases;
 
@@ -32,7 +34,11 @@ class PushNotificationBloc extends BaseBloc {
   FutureOr<void> _onConfigPushNotification(OnConfigNotification event, Emitter<BaseState> emit) async {
     blocCommunication.configNotiReceivers(event);
     await handleUsecaseResult(
-      usecaseResult: _pushNotiUseCases.config(),
+      usecaseResult: _pushNotiUseCases.config(PushNotificationConfig(
+        onNotiSelected: (PushNotificationPayload payload) {
+          add(OnPushNotificationPressed(data: payload));
+        },
+      )),
       emit: emit,
       showErrorWhenFail: false,
     );
@@ -45,5 +51,9 @@ class PushNotificationBloc extends BaseBloc {
       emit: emit,
       showErrorWhenFail: false,
     );
+  }
+
+  FutureOr<void> _onPushNotificationPressed(OnPushNotificationPressed event, Emitter<BaseState> emit) {
+    emit(PushNotificationPressed(event.data));
   }
 }
