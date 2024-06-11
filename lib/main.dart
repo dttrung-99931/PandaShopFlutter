50aab68a0ad7f552ed32c1d0b161e861b0ce0831
ptt <ptt99931.work@gmail.com>
2024-06-11 21:47:43 +0700

    [map] use separated panda_map, here_panda_map packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:evievm_app/src/config/app_config.dart';
 import 'package:evievm_app/app.dart';
+import 'package:flutter/widgets.dart';
 
 void main() async {
   configProduction();
@@ -7,6 +8,8 @@ void main() async {
 }
 
 void configProduction() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO: config PandaMap
   AppConfig.set(
     appName: 'EVM',
     flavorName: AppFlavor.PRODUCTION,
